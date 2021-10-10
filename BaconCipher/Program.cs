d4c59b52ae0d4510868a1ad824deb69c643ebf07
Kevin <kevinleo.lucas@gmail.com>
2021-10-10 18:55:11 -0400

    initial setup

---
+++ BaconCipher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaconCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
